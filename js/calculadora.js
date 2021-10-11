8018e4614606f1075f2e081cfb31d8dd8804e790
Gabriel Baruque <baruque@edu.unirio.br>
2021-10-11 20:27:22 -0300

    implementaErro

--- js/calculadora.js
+++ js/calculadora.js
@@ -20,9 +20,16 @@ function subtrair()
     n1 = document.getElementById('n1').value;
     n2 = document.getElementById('n2').value;
 
-    resultado = Number.parseInt(n1) - Number.parseInt(n2);
-
-    window.alert(resultado);
+    let msgError = document.getElementsByClassName('error')
+    if(!validar(n1,n2))
+    {
+        msgError[1].innerHTML = "Um ou mais campos na subtração está em branco.";
+    } else
+    {   
+        msgError[1].innerHTML = "";
+        resultado = Number.parseInt(n1) - Number.parseInt(n2);
+        window.alert(resultado);   
+    }
 }
 
 function multiplicar()
@@ -30,9 +37,16 @@ function multiplicar()
     n1 = document.getElementById('n1').value;
     n2 = document.getElementById('n2').value;
 
-    resultado = Number.parseInt(n1) * Number.parseInt(n2);
-
-    window.alert(resultado);
+    let msgError = document.getElementsByClassName('error')
+    if(!validar(n1,n2))
+    {
+        msgError[2].innerHTML = "Um ou mais campos na multiplicação está em branco.";
+    } else
+    {   
+        msgError[2].innerHTML = "";
+        resultado = Number.parseInt(n1) * Number.parseInt(n2);
+        window.alert(resultado);   
+    }
 }
 
 function dividir()
@@ -40,9 +54,16 @@ function dividir()
     n1 = document.getElementById('n1').value;
     n2 = document.getElementById('n2').value;
 
-    resultado = Number.parseInt(n1) / Number.parseInt(n2);
-
-    document.getElementById('calculo').value = resultado
+    let msgError = document.getElementsByClassName('error')
+    if(!validar(n1,n2))
+    {
+        msgError[3].innerHTML = "Um ou mais campos na divisão está em branco.";
+    } else
+    {   
+        msgError[3].innerHTML = "";
+        resultado = Number.parseInt(n1) / Number.parseInt(n2);
+        window.alert(resultado);   
+    }
 }
 
 function media()
@@ -51,14 +72,31 @@ function media()
     n2_media = document.getElementById('n2_media').value;
     n3_media = document.getElementById('n3_media').value;
 
-    resultado = (Number.parseInt(n1_media) + Number.parseInt(n2_media) + Number.parseInt(n3_media)) / 3;
-    
-    document.getElementById('mediaFinal').value = resultado.toFixed(2)
+    let msgError = document.getElementsByClassName('error')
+    if(!validarMedia(n1,n2,n3))
+    {
+        msgError[4].innerHTML = "Um ou mais campos na média está em branco.";
+    } else
+    {   
+        msgError[4].innerHTML = "";
+        resultado = (Number.parseInt(n1_media) + Number.parseInt(n2_media) + Number.parseInt(n3_media)) / 3;
+        document.getElementById('mediaFinal').value = resultado.toFixed(2)  
+    }    
 }
 
 function validar(n1, n2)
 {
     if(n1.trim() === "" || n2.trim() === "")
+    {
+        return false;
+    } else {
+        return true
+    }
+}
+
+function validarMedia(n1, n2, n3)
+{
+    if(n1.trim() === "" || n2.trim() === "" || n3.trim() === "")
     {
         return false;
     } else {
