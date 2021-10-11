261cb203cbad994ad49e5483c8b08dbc9bd9ca7f
Gabriel Baruque <baruque@edu.unirio.br>
2021-10-11 19:52:37 -0300

    ErroSoma

--- js/calculadora.js
+++ js/calculadora.js
@@ -3,9 +3,16 @@ function somar()
     n1 = document.getElementById('n1').value;
     n2 = document.getElementById('n2').value;
 
-    resultado = Number.parseInt(n1) + Number.parseInt(n2);
-
-    window.alert(resultado);
+    let msgError = document.getElementsByClassName('error')
+    if(!validar(n1,n2))
+    {
+        msgError[0].innerHTML = "Um ou mais campos na soma está em branco.";
+    } else
+    {   
+        msgError[0].innerHTML = "";
+        resultado = Number.parseInt(n1) + Number.parseInt(n2);
+        window.alert(resultado);   
+    }
 }
 
 function subtrair()
@@ -40,11 +47,21 @@ function dividir()
 
 function media()
 {
-    n1 = document.getElementById('n1_media').value;
-    n2 = document.getElementById('n2_media').value;
-    n3 = document.getElementById('n3_media').value;
+    n1_media = document.getElementById('n1_media').value;
+    n2_media = document.getElementById('n2_media').value;
+    n3_media = document.getElementById('n3_media').value;
 
-    resultado = (Number.parseInt(n1) + Number.parseInt(n2) + Number.parseInt(n3)) / 3;
+    resultado = (Number.parseInt(n1_media) + Number.parseInt(n2_media) + Number.parseInt(n3_media)) / 3;
     
     document.getElementById('mediaFinal').value = resultado.toFixed(2)
+}
+
+function validar(n1, n2)
+{
+    if(n1.trim() === "" || n2.trim() === "")
+    {
+        return false;
+    } else {
+        return true
+    }
 }
